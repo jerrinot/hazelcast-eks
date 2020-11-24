3bfe0f22d2741a807f61cff7da407b875316d2aa
jerrinot <jaromir.hamala@gmail.com>
2020-11-24 11:39:53 +0100

    check statefulset and config map dont exist before deploying

--- deploy-hazelcast.sh
+++ deploy-hazelcast.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
+hz_depl=$(kubectl get statefulsets.apps -o name|grep hazelcast)
+if [ "$hz_depl" = "statefulset.apps/hazelcast" ]; then
+   echo Hazelcast statefulset is already deployed. Undeploy first and then run this script again
+   exit 1   
+fi
+
+hz_config=$(kubectl get configmaps -o name|grep hazelcast)
+if [ "$hz_config" = "configmap/hazelcast-configuration" ]; then
+  echo Hazelcast config map is already deployed. Undeplay first and then run this script again	
+  exit 1
+fi
+
+nodes=$(kubectl get nodes -o name|wc -l)
+echo Deploying Hazelcast to $nodes nodes
 kubectl create configmap hazelcast-configuration --from-file=hazelcast-config.yaml
 kubectl apply -f hazelcast-statefulset.yaml
+echo Done
